df887cc0014e1f6450de44bdf05077b84fc3c1e8
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-06-27 15:22:25 +0800

    Create index.php

---
+++ AptitudeExam11/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration Form</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <h1>Registration Form</h1>
+  <?php
+  include 'assets/partials/form.php';
+  ?>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="assets/js/validation.js"></script>
+</body>
+</html>
